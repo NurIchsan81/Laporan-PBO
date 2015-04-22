8c28a1c0ee770c996a149bc21ee2a7f5f741078f
NurIchsan81 <nur.ichsan.amin@gmail.com>
2015-04-22 10:31:43 +0800

    Create coba.java
    1 class dan menampilkan biodata dan perhitungan

---
+++ coba.java
@@ -0,0 +1,33 @@
+package baru;
+
+public class coba {
+	static String coba = "Praktikum Perdana";
+	public static void main (String[] args) {
+	System.out.println("baru");
+	judul();
+	pembagian();
+	biodata();
+}
+	public static void judul()
+	{
+		System.out.println("Praktikum 1");
+	}
+	
+	public static void pembagian()
+	{
+		int a = 8/2 ;
+		float b= 9/2 ;
+		System.out.println("pembagian1 =" +a);
+		System.out.println("pembagian2 =" +b);
+	}
+	
+	public static void biodata ()
+	{
+		String nama= "Iccang";
+		String alamat= "Jln. Baji Gau";
+		System.out.println("nama saya = "+nama + " alamat = "+alamat);
+		
+	}
+}
+
+
